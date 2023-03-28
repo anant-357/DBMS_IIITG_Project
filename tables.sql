bdfcc5e775f08dec49b21af98684ded4f14a1e47
anant <95141712+anant-357@users.noreply.github.com>
2023-03-28 11:30:08 +0530

    Updating Sells Primary key to P_ID

--- tables.sql
+++ tables.sql
@@ -61,8 +61,10 @@ CREATE TABLE
         P_ID INTEGER,
         FOREIGN KEY (Seller_ID) REFERENCES Sellers(Seller_ID),
         FOREIGN KEY (P_ID) REFERENCES Properties(P_ID),
-        PRIMARY KEY (Seller_ID, P_ID)
+        PRIMARY KEY (P_ID)
     );
+
+DROP TABLE Sells;
 
 CREATE TABLE
     Shows(
@@ -82,61 +84,147 @@ CREATE TABLE
     );
 
 -- Clients table
-INSERT INTO Clients (Name, Phone_no) VALUES
-('John Doe', 1234567890),
-('Jane Doe', 2345678901),
-('Alice Smith', 3456789012),
-('Bob Johnson', 4567890123),
-('Emily Davis', 5678901234);
+
+INSERT INTO
+    Clients (Name, Phone_no)
+VALUES ('John Doe', 1234567890), ('Jane Doe', 2345678901), ('Alice Smith', 3456789012), ('Bob Johnson', 4567890123), ('Emily Davis', 5678901234);
 
 -- Properties table
-INSERT INTO Properties (Address, Locality, Area, Price, Rent, Date_Of_Construction, No_Of_Bedrooms, Status, Sell_Date, Sell_Price) VALUES
-('123 Main St', 'West Boulevard', 1000, 500000, FALSE, '2020-01-01', 2, 'Available', NULL, NULL),
-('456 Oak Ave', 'Suburbia', 1500, 750000, FALSE, '2010-01-01', 3, 'Available', NULL, NULL),
-('789 Elm St', 'Rural', 2000, 1000000, FALSE, '2005-01-01', 4, 'Sold', NULL, NULL),
-('321 Maple St', 'Downtown', 800, 400000, TRUE, '2015-01-01', 1, 'Sold', NULL, NULL),
-('654 Pine Ave', 'Borjhar', 1200, 600000, TRUE, '2012-01-01', 2, 'Available', NULL, NULL);
+
+INSERT INTO
+    Properties (
+        Address,
+        Locality,
+        Area,
+        Price,
+        Rent,
+        Date_Of_Construction,
+        No_Of_Bedrooms,
+        Status,
+        Sell_Date,
+        Sell_Price
+    )
+VALUES (
+        '123 Main St',
+        'West Boulevard',
+        1000,
+        500000,
+        FALSE,
+        '2020-01-01',
+        2,
+        'Available',
+        NULL,
+        NULL
+    ), (
+        '456 Oak Ave',
+        'Suburbia',
+        1500,
+        750000,
+        FALSE,
+        '2010-01-01',
+        3,
+        'Available',
+        NULL,
+        NULL
+    ), (
+        '789 Elm St',
+        'Rural',
+        2000,
+        1000000,
+        FALSE,
+        '2005-01-01',
+        4,
+        'Sold',
+        NULL,
+        NULL
+    ), (
+        '321 Maple St',
+        'Downtown',
+        800,
+        400000,
+        TRUE,
+        '2015-01-01',
+        1,
+        'Sold',
+        NULL,
+        NULL
+    ), (
+        '654 Pine Ave',
+        'Borjhar',
+        1200,
+        600000,
+        TRUE,
+        '2012-01-01',
+        2,
+        'Available',
+        NULL,
+        NULL
+    );
 
 -- Sellers table
-INSERT INTO Sellers (Name, Phone_no) VALUES
-('Tom Johnson', 6789012345),
-('Samantha Lee', 7890123456),
-('James Smith', 8901234567),
-('Emma Wilson', 9012345678),
-('Mike Brown', 1234567890);
+
+INSERT INTO
+    Sellers (Name, Phone_no)
+VALUES ('Tom Johnson', 6789012345), ('Samantha Lee', 7890123456), ('James Smith', 8901234567), ('Emma Wilson', 9012345678), ('Mike Brown', 1234567890);
 
 -- Brokers table
-INSERT INTO Brokers (Name, Phone_no, Brokerage, Locality) VALUES
-('Bill Johnson', 2345678901, 1, 'Downtown'),
-('Linda Davis', 3456789012, 2, 'Borjhar'),
-('Alex Lee', 4567890123, 3, 'Rural'),
-('Grace Wilson', 5678901234, 4, 'West Boulevard'),
-('Jake Brown', 6789012345, 5, 'Suburbia');
+
+INSERT INTO
+    Brokers (
+        Name,
+        Phone_no,
+        Brokerage,
+        Locality
+    )
+VALUES (
+        'Bill Johnson',
+        2345678901,
+        1,
+        'Downtown'
+    ), (
+        'Linda Davis',
+        3456789012,
+        2,
+        'Borjhar'
+    ), (
+        'Alex Lee',
+        4567890123,
+        3,
+        'Rural'
+    ), (
+        'Grace Wilson',
+        5678901234,
+        4,
+        'West Boulevard'
+    ), (
+        'Jake Brown',
+        6789012345,
+        5,
+        'Suburbia'
+    );
 
 -- Holds table
-INSERT INTO Holds (Client_ID, P_ID) VALUES
-(1, 3),
-(2, 4);
+
+INSERT INTO Holds (Client_ID, P_ID) VALUES (1, 3), (2, 4);
 
 -- Sells table
-INSERT INTO Sells (Seller_ID, P_ID) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4),
-(5, 5);
 
+INSERT INTO
+    Sells (Seller_ID, P_ID)
+VALUES (1, 1), (2, 2), (3, 3), (4, 4), (5, 5);
 
 DELIMITER //
-CREATE TRIGGER add_show
-AFTER INSERT ON Properties
-FOR EACH ROW
-BEGIN
-    INSERT INTO Shows (License_ID, P_ID)
-    SELECT License_ID, NEW.P_ID
-    FROM Brokers
-    WHERE Locality = NEW.Locality;
-END //
+
+CREATE TRIGGER ADD_SHOW AFTER INSERT ON PROPERTIES 
+FOR EACH ROW BEGIN 
+	INSERT INTO
+	    Shows (License_ID, P_ID)
+	SELECT License_ID, NEW.P_ID
+	FROM Brokers
+	WHERE Locality = NEW.Locality;
+	END // 
+
+
 DELIMITER ;
 
 SHOW TRIGGERS;
@@ -144,8 +232,7 @@ SHOW TRIGGERS;
 SELECT * FROM `Shows`;
 
 TRUNCATE TABLE Shows;
+
 SET foreign_key_checks = 1;
+
 TRUNCATE TABLE Properties;
-
-
-
