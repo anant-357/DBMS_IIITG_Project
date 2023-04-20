fff7a97ef4d661c0bf04e823ccc2073be9b84bb0
anant <95141712+anant-357@users.noreply.github.com>
2023-04-20 14:55:14 +0530

    updated css and js

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,8 @@ const registerLink = document.querySelector(".register-link");
 const btnPopup = document.querySelector(".btnLogin-popup");
 const iconClose = document.querySelector(".icon-close");
 const navbar = document.querySelector(".navbar");
+const navbarElement = document.getElementById("navbar");
+const navbarHeight = navbarElement.offsetHeight;
 let position = null;
 
 registerLink.addEventListener("click", () => {
@@ -14,22 +16,24 @@ loginLink.addEventListener("click", () => {
   wrapper.className = "wrapper active-popup";
 });
 
-btnPopup.addEventListener("click", () => {
-  const wrapperIsActive = wrapper.classList.contains("active");
-  const wrapperIsPopupActive = wrapper.classList.contains("active-popup");
-  const targetPosition =
-    wrapperIsActive && !wrapperIsPopupActive
-      ? navbar.getBoundingClientRect().top
-      : position;
+if (btnPopup) {
+  btnPopup.addEventListener("click", () => {
+    const wrapperIsActive = wrapper.classList.contains("active");
+    const wrapperIsPopupActive = wrapper.classList.contains("active-popup");
+    const targetPosition =
+      wrapperIsActive && !wrapperIsPopupActive
+        ? navbar.getBoundingClientRect().top
+        : position;
 
-  window.scrollTo({
-    top: targetPosition,
-    behavior: "smooth",
+    window.scrollTo({
+      top: targetPosition,
+      behavior: "smooth",
+    });
+
+    wrapper.classList.toggle("active-popup");
+    position = wrapperIsPopupActive ? position : window.scrollY;
   });
-
-  wrapper.classList.toggle("active-popup");
-  position = wrapperIsPopupActive ? position : window.scrollY;
-});
+}
 
 iconClose.addEventListener("click", () => {
   window.scrollTo({
@@ -38,4 +42,12 @@ iconClose.addEventListener("click", () => {
   });
 
   wrapper.classList.remove("active-popup");
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= navbarHeight) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 });
