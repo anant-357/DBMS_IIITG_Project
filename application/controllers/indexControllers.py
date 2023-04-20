65ba43c8b851af3bbe98f0a8157295d50c042129
anant <95141712+anant-357@users.noreply.github.com>
2023-04-20 14:52:37 +0530

    starting search box

--- application/controllers/indexControllers.py
+++ application/controllers/indexControllers.py
@@ -1,19 +1,36 @@
 from flask import current_app as app, session, request, url_for, redirect, flash
 from flask import render_template
-from application.models import Properties, isValidUser, Photos, Sells
+from application.models import Properties, isValidUser, Photos, Sells, Brokers
 from application.database import db
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "GET":
-        properties = Properties.query.join(Photos).all()
+        properties = (
+            Properties.query.filter(Properties.Status == "Available").join(Photos).all()
+        )
+        localities = Brokers.query.with_entities(Brokers.Locality.distinct()).all()
+        areas = Properties.query.with_entities(Properties.Area.distinct()).all()
+        min_area = min(areas)[0]
+        max_area = max(areas)[0]
+        diff = (max_area - min_area) // 5
+        areas = list(range(min_area, max_area + 1, diff))
+        print(areas)
+
         if "badlogin" in session.keys():
             session.pop("badlogin")
             flash("Either userID or Password is wrong!")
-        return render_template("home/index.html", properties=properties)
+        return render_template(
+            "home/index.html", properties=properties, localities=localities, areas=areas
+        )
+    else:
+        return redirect(url_for("home"))
 
-    elif request.method == "POST":
+
+@app.route("/login", methods=["POST"])
+def login():
+    if request.method == "POST":
         user_id = request.form["userID"]
         password = request.form["password"]
         type = request.form["type"]
@@ -22,9 +39,12 @@ def home():
             session["userID"] = user_id
             session["type"] = type
             session["username"] = validity[1]
-            return redirect(url_for("home"))
         else:
             session["badlogin"] = True
+        if session["type"] == "Admin":
+            return redirect(url_for("admin"))
+        return redirect(url_for("home"))
+    else:
         return redirect(url_for("home"))
 
 
@@ -92,5 +112,16 @@ def addProperty():
 
         else:
             return redirect(url_for("home"))
+    else:
+        return redirect(url_for("home"))
+
+
+@app.route("/search", methods=["POST"])
+def search():
+    min_price = None
+    max_price = None
+    if request.method == "POST":
+        locality = request.form["locality"]
+
     else:
         return redirect(url_for("home"))
